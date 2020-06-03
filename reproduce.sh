571a60250eb53e6f4c6828213393515cf7643fd0
Jack Turner <jackwilliamturner@icloud.com>
2020-06-03 13:01:58 +0100

    Update reproduce.sh

--- reproduce.sh
+++ reproduce.sh
@@ -1,4 +1,4 @@
-python search.py --dataset cifar10
-python search.py --dataset cifar10 --trainval
-python search.py --dataset cifar100
-python search.py --dataset ImageNet16-120
+python search.py --dataset cifar10 --data_loc '../datasets/cifar10'
+python search.py --dataset cifar10 --trainval --data_loc '../datasets/cifar10'
+python search.py --dataset cifar100 --data_loc '../datasets/cifar100'
+python search.py --dataset ImageNet16-120 --data_loc '../datasets/ImageNet16'
